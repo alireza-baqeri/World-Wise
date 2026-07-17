1a2aae970249939e4a582ef232f856af200bbfb4
Alireza <96381136+alireza-baqeri@users.noreply.github.com>
2026-07-17 13:43:13 +0330

    adding the fundamental structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 function App(){
-
-  return <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Homepage/>}/>
-  <Route path="product" element={<Product />}/>
-  <Route path='pricing' element={<Pricing/>}/>
-  <Route path='*' element={<PageNotFound/>}/>
-  </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App

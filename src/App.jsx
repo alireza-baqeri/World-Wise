f4022a3484066ae1df7795073ab948a05e8977e6
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-08-09 05:22:42 +0330

    url-base-state and Programmatic Navigation with useNavigate is set

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,67 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout"
+import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form"
 
-const BASE_URL = 'http://localhost:9000'
+const BASE_URL = "http://localhost:9000";
 
 function App() {
-  const [cities, setCities] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function fetchCities() {
       try {
-        setIsLoading(true)
+        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data)
+        setCities(data);
       } catch {
-              alert("There wsa an error loading data ...")
+        alert("There wsa an error loading data ...");
       } finally {
-        setIsLoading(false)
+        setIsLoading(false);
       }
     }
     fetchCities();
-  },[])
+  },
+[]);
 
   return (
     <BrowserRouter>
       <Routes>
-         <Route index element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login/>} />
-        <Route path="app" element={<AppLayout />} >
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />}/>
-          <Route path="countries" element={<p>Countries</p>} />
-          <Route path="form" element={<p>From</p>} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
 
-      </Route>
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path='cities/:id' element={<City/>}/>
+          <Route
+            path="countries"
+            element={
+              <p>
+                <CountryList cities={cities} isLoading={isLoading} />
+              </p>
+            }
+          />
+          <Route path="form" element={<Form/>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,41 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+
+    const navigate = useNavigate()
+
+    const [searchParams, setSearchParams] = useSearchParams();
+   
+    const lat = searchParams.get("lat")
+    const lng = searchParams.get("lng")
+    console.log({ lat });
+    
     return (
-        <div className={styles.mapContainer}>
-            Map
-        </div>
-    )
+      <div className={styles.mapContainer} onClick={()=>{navigate("form")}}>
+        <h1>Map</h1>
+
+        <h1>
+          Position:
+          <p>
+            {" "}
+            {lat},
+            {lng}
+          
+          </p>
+         
+        </h1>
+
+        <button
+          onClick={() => {
+            setSearchParams({ lat: 23, lng: 50 });
+          }}
+        >
+          Change Position
+        </button>
+        
+      </div>
+    );
 }
 
 export default Map

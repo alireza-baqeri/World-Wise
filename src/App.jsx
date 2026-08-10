f369ef6f1773cb57179a1a1beefb4b4cd52a6502
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-08-10 03:56:37 +0330

    React router for the project is fully set now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import Product from "./pages/Product";
@@ -43,16 +43,13 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to="cities" replace/>} />
 
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path='cities/:id' element={<City/>}/>
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={
@@ -61,7 +58,7 @@ function App() {
               </p>
             }
           />
-          <Route path="form" element={<Form/>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>

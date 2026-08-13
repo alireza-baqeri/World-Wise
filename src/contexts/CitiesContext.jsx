e217bbc081e23c8a90a235dbf69c306b366e03a6
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-08-13 14:52:54 +0330

    with all hardness it had I could finally setup the back button for city form and cities list, I could add the active city style to the active city component and I recive the city list from the api now not the dummy local obj

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -2,38 +2,56 @@ import { createContext, useState, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
-
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
-    useEffect(function () {
-      async function fetchCities() {
-        try {
-          setIsLoading(true);
-          const res = await fetch(`${BASE_URL}/cities`);
-          const data = await res.json();
-          setCities(data);
-        } catch {
-          alert("There wsa an error loading data ...");
-        } finally {
-          setIsLoading(false);
-        }
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There wsa an error loading data ...");
+      } finally {
+        setIsLoading(false);
       }
-      fetchCities();
-    }, []);
+    }
+    fetchCities();
+  }, []);
 
-    return <CitiesContext.Provider value={{cities,isLoading}}>
-        {children}
+   async function getCity(id) {
+     try {
+       setIsLoading(true);
+       const res = await fetch(`${BASE_URL}/cities/${id}`);
+       const data = await res.json();
+       setCurrentCity(data);
+     } catch {
+       alert("There wsa an error loading data ...");
+     } finally {
+       setIsLoading(false);
+     }
+   }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
     </CitiesContext.Provider>
+  );
 }
+ 
+
 
 function useCities() {
-    const context = useContext(CitiesContext)
-    if(context === undefined) throw new Error("CitiesContext was used outside the CitiesProvider")
-    return context
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
 }
 
-export {CitiesProvider, useCities}
+export { CitiesProvider, useCities };
